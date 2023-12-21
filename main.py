373dcc73bd8df4bd20347e2ba45217eefd693108
Quentin Goujon <quentin.goujon@epita.fr>
2023-12-21 10:48:37 +0100

    [UPDATE] add a parser

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from camion import Camion
 from produit import Produit
+from parser import Parser
 
-my_produit = Produit(2, 1, 3)
+chemin_fichier = 'input.sample'
+parser = Parser(chemin_fichier)
+print(parser)
 
-my_camion = Camion(4, 4, 4)
 
-my_camion.placer_produit(my_produit, 0, 0, 0, 2, 1, 3)
 
-print(my_camion)
 
--- parser.py
+++ parser.py
@@ -1,1 +1,36 @@
-# lire input.sample et transformer en liste de produit :) et recup valeur des camions
+from produit import Produit
+
+class Parser:
+    def __init__(self, chemin_fichier):
+        self.chemin_fichier = chemin_fichier
+
+        self.longueur_camion = 0
+        self.largeur_camion = 0
+        self.hauteur_camion = 0
+
+        self.liste_produits = []
+        self.parse_fichier()
+
+    def parse_fichier(self):
+        with open(self.chemin_fichier, 'r') as file:
+            dimensions_camion = list(map(int, file.readline().split()))
+            self.longueur_camion, self.largeur_camion, self.hauteur_camion = dimensions_camion
+
+            nb_produits = int(file.readline())
+
+            for _ in range(nb_produits):
+                dimensions_produit = list(map(int, file.readline().split()))
+                produit = Produit(*dimensions_produit)
+                self.liste_produits.append(produit)
+
+    def __str__(self):
+        print(f"Dimensions du camion : {self.longueur_camion} x {self.largeur_camion} x {self.hauteur_camion}")
+        print("Liste des produits :")
+        for produit in self.liste_produits:
+            print(produit)
+        return ""
+
+# if __name__ == "__main__":
+#     chemin_fichier = 'input.sample'
+#     parser = Parser(chemin_fichier)
+#     print(parser)
