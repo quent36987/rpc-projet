0f539b40180a70f71bdefc2ad21b15f6a21d87e3
Quentin Goujon <quentin.goujon@epita.fr>
2023-12-21 11:18:48 +0100

    [FIX] name and truck

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ chemin_fichier = 'input.sample'
 parser = Parser(chemin_fichier)
 print(parser)
 
+truck = Truck(parser.longueur_camion, parser.largeur_camion, parser.hauteur_camion)
+truck.place_product(parser.liste_produits[0], 0, 0, 0, 1, 1, 1)
+print(truck)
+
 
 
 
